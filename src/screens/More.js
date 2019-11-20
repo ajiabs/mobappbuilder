6119affb679037323eacc279471ea129e67cd3ca
Jain <jain.j@armiasystems.com>
2019-11-20 11:06:23 +0530

    BASIC SETTINGS

--- src/screens/More.js
+++ src/screens/More.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import { BuildConfig } from '../config';
 
 export default class More extends React.Component {
   render() {
+    const backgroundColorStyles = {
+      backgroundColor:  BuildConfig.background_color
+    };
+    const textColorStyles = {
+      color:  BuildConfig.text_color
+    };
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>More Screen</Text>
