2a9d84a638c5a3c7baa78e9fa612f2a273b26d9f
Anoopvt <anoop.v@armiasystems.com>
2020-01-14 12:51:52 +0530

    edited
    Signed-off-by: Anoopvt <anoop.v@armiasystems.com>

--- src/screens/Event-details.js
+++ src/screens/Event-details.js
@@ -1,13 +1,66 @@
 import React from 'react';
-import { View, Text ,Dimensions,ScrollView,Image} from 'react-native';
+import { View, Text ,Dimensions,ScrollView,Image,TouchableOpacity,Button,Alert} from 'react-native';
 import { Styles } from '../assets/css/styles';
 import { AssetsImages } from '../assets/images';
 import { BuildConfig } from '../config';
+import RNPaypal from 'react-native-paypal-lib';
 let { width } = Dimensions.get('window');
 export default class EventDetails extends React.Component {
   constructor(props) {
     super(props);
   
+  }
+  simpleAlertHandlerSuccess=()=>{
+    //function to make simple alert
+    Alert.alert(
+      //title
+      'Payment Success',
+      //body
+      'Thank You for Funding the Event.',
+      [
+        
+        {text: 'OK', onPress: () => console.log('No Pressed'), style: 'cancel'},
+      ],
+      { cancelable: false }
+      //clicking out side of alert will not cancel
+    );
+  }
+  simpleAlertHandlerError=()=>{
+    //function to make simple alert
+    Alert.alert(
+      //title
+      'Payment Failed!',
+      //body
+      'Payment Failed,Please try Later.',
+      [
+        
+        {text: 'OK', onPress: () => console.log('No Pressed'), style: 'cancel'},
+      ],
+      { cancelable: false }
+      //clicking out side of alert will not cancel
+    );
+  }
+  paypalCall = () => {
+    //alert("Here");
+    RNPaypal.paymentRequest({
+      clientId: 'AVXDyBglm3jiy6S9y-zUFAdJpk_NAesoyEHSLTp1B3OQtBb6yZm3ZhyH9tL-4WM8SfbiPhJ9nWKvdGVc',
+      environment: RNPaypal.ENVIRONMENT.SANDBOX,
+      intent: RNPaypal.INTENT.SALE,
+      price: 60,
+      currency: 'USD',
+      description: `Android testing`,
+      acceptCreditCards: true
+  }).then(response => {
+      console.log(response.response.state);
+      if(response.response.state=="approved" && response.response.id){
+        this.simpleAlertHandlerSuccess();
+      }else{
+        this.simpleAlertHandlerError();
+      }
+     
+  }).catch(err => {
+      console.log(err.message)
+  })
   }
 
   render() {
@@ -33,6 +86,15 @@ export default class EventDetails extends React.Component {
               <Image  source={AssetsImages.time}  style={[Styles.event_row_image]}/>
               <Text style={[Styles.event_row_text,Styles.page_text_color]}>{navigation.getParam('time')}</Text>
             </View>
+
+
+            <Button
+            title="Fund This Event"
+            onPress={() => this.paypalCall()}
+          />
+         
+
+
              </ScrollView>
          </View>
 
--- src/screens/Events.js
+++ src/screens/Events.js
@@ -4,43 +4,9 @@ import { Styles } from '../assets/css/styles';
 import { AssetsImages } from '../assets/images';
 import { BuildConfig } from '../config';
 import {Calendar} from 'react-native-calendars';
+import moment from 'moment';
 let { width } = Dimensions.get('window');
-var Data = [
-  {
-    start_date: '2019-12-02',
-    time: '01:00-09:00',
-    title: 'AMERICAN FILM AWARDS® 2019 ',
-    location:'Regal LA Live & 4DX ,1000 West Olympic Boulevard Los Angeles,',
-    summary: 'PLAY FESTIVAL (12pm to 3pm) - Showcasing new Recording Artists, Producers, Beatmakers, Models, Fashion Designers, Comedians, Scripwriters, Influencers, Gamers, and Content Creators - meeting with Brands, Buyers and Investors + Entertainment Panels and Networking.Red Carpet Arrivals for Gala (5pm -7pm)',
-  },{
-    start_date: '2019-12-03',
-    time: '07:00-11:00',
-    title: '2019 Music & Art Exhibit by Woven Colors',
-    location:'Hot and Cool Cafe 4331 Degnan Boulevard Los Angeles',
-    summary: 'We are here for the community of creators of all kind! Come support local artist in the music industry and visual arts. This event will bring together professionals in the arts & entertainment industry among others to enjoy music and art in one place.Look forward to a evening filled with performances, spoken word, art gallery, music listening, and networking.Art work will available for purchase.',
-  },
-  {
-    start_date: '2019-12-02',
-    time: '07:00-11:00',
-    title: 'Emerging Tech - Digital Humans & Virtual Production',
-    location:'The Scan Truck - Mobile 3D Scanning Los Angeles 11160 Hindry Avenue Los Angeles',
-    summary: 'The ways in which we create content are changing rapidly, and the line between digital and real becoming blurred. New tools have emerged in the entertainment industry and beyond to represent characters digitally and create content with them through virtual production - increasing efficiency and reducing costs.',
-  },
-  {
-    start_date: '2019-12-03',
-    time: '08:00-12:00',
-    title: 'Content Protection Summit 2019',
-    location:'Sheraton Universal Hotel 333 Universal Hollywood Dr.Universal City,',
-    summary: 'Industry leaders from the Hollywood studios, television networks and content production community are again scheduled to meet at the 10th Annual CDSA Content Protection Summit on Wednesday, December 4, 2019 at the Sheraton Universal. The full-day conference program will address the dynamic challenges throughout the entertainment supply chain in managing risk, thwarting attacks and educating their teams while protecting their business practices and customers’ privacy.',
-  },
-  {
-    start_date: '2019-12-03',
-    time: '01:00-10:00',
-    title: 'The Sound Bar experience ',
-    location:'7080 Hollywood Blvd 7080 Hollywood Boulevard Los Angeles',
-    summary: 'MIXER - Exclusive to influencers/brand ambassadors & Sponsors,Artists • Podcasters • DJs • Influencers • Hype beast • Models • etc,Apparel • Restaurants • Tech • Cannabis • Music • All industries Current influencer follower count: 3 Million (Updated every week) Current Sponsor budget: 25 influencers (Updated every week)',
-  },
-];
+var Data = [];
 
 
 function Item({ item ,navigation}) {
@@ -62,10 +28,12 @@ function Item({ item ,navigation}) {
      );
 }
 
-function fetchEvents(date){
+function fetchEvents(date,d){
   var searched_date = date.year+'-'+date.month+'-'+ ("0" + date.day).slice(-2); 
-
-  var res = Data.filter((item)=>{
+  console.log(searched_date,"searched_date");
+  console.log(d,"d");
+  
+  var res = d.filter((item)=>{
     return Object.keys(item).some((key)=>item[key].includes(searched_date));
   });
   return {result:res,date:searched_date};
@@ -77,31 +45,139 @@ export default class Events extends React.Component {
     super(props);
     this.state = { 
       DataItem: Data,
-      selected_date:{year:2019,month:12,day:3}
+      selected_date:{year:2019,month:12,day:3},
+      resEvent:{value:"Alabama"},
+      markedDates:{},
+      currentDate:""
     }; 
-    
   
   }
 
   
  getEvents(date){
-    var res = fetchEvents(date);
+ 
+ this.state.currentDate = date.dateString;
+
+    var res = fetchEvents(date,this.state.resEvent.value);
+   
     this.setState({ 
       DataItem:  res.result,
-      selected_date:res.date
-    
+      selected_date:res.date,
+      currentDate:date.dateString
     }); 
 
   }
 
   componentDidMount() {
     
-    var res = fetchEvents(this.state.selected_date);
+    var todayDate   =   moment().format('YYYY-MM-DD');
+    var todayDate2  = moment().format('YYYY-M-DD');
+
+    this.state.selected_date.year = moment().format('YYYY');
+    this.state.selected_date.month = moment().format('M');
+    this.state.selected_date.day = moment().format('DD');
+    
+    this.state.currentDate = todayDate;
+    var data = {
+      token_id: BuildConfig.token_id,
+    };
+    return fetch('https://mobapp.iscriptsdemo.com/api/events/getEvents',
+    {
+    method: "POST",
+    headers: {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+       
+        
+
+        var resData = [];
+        var setDate = {};
+        var newsetDate = {};
+        
+       
+        for (let eventObject of responseJson) {
+          
+ 
+          
+
+
+          var mydate = (eventObject['event_start_date']);
+          mydate = mydate.split("/");
+          var formatDate = mydate[2]+'-'+mydate[1]+'-'+mydate[0];
+
+          var d1 = moment(new Date(formatDate)).format('YYYY-MM-DD');
+          var d2 = moment(new Date(formatDate)).format('YYYY-M-DD');
+
+         
+
+          var date = d1;
+          var date1 = d2;
+          var newDate = date;
+          var newDate1 = date1;
+          setDate[newDate] = {selected: true};
+
+          var obj = {
+            start_date: newDate1,
+            time: eventObject['event_start_time'],
+            title: eventObject['event_title'],
+            location:eventObject['location'],
+            summary: eventObject['event_summary'],
+          }
+          
+
+        
+          
+          resData.push(obj);
+
+      }
+
+     
+
+      this.setState({
+         resEvent:{value:resData},
+         markedDates:setDate
+      }, function(){
+
+        console.log("fetchEvents");
+        var res = fetchEvents(this.state.selected_date,this.state.resEvent.value);
+        this.setState({ 
+          DataItem:  res.result,
+          selected_date:res.date,
+         
+        }); 
+      });
+     
+      
+      
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+
+
+      
+    
+
+
+
+    
+    
+   
+
     this.setState({ 
       DataItem:  res.result,
-      selected_date:res.date
+      selected_date:res.date,
+     
     
     }); 
+    
   
 
   }
@@ -124,21 +200,18 @@ export default class Events extends React.Component {
             selectedDayBackgroundColor: 'red',
       
             }}
-          markedDates={{
-              '2019-12-02': {selected: true},
-              '2019-12-03': {selected: true},
-            
-           
-            }}
+          markedDates={this.state.markedDates}
                     
           // Initially visible month. Default = Date()
-          current={'2019-12-02'}
+          current={this.state.currentDate}
           // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
           minDate={'2019-01-01'}
           // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
           maxDate={'2030-12-31'}
           // Handler which gets executed on day press. Default = undefined
           onDayPress={(date) => {this.getEvents(date)}}
+
+          
 
           // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
           monthFormat={'MMM yyyy'}
